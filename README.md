1800772843afea91d846e5cc605d5aed64a0842c
Mickael Allonneau <mickael.allonneau@gmail.com>
2014-03-28 12:59:46 +0100

    Renamed project from 'PermalinkJS' to 'HotlinkJS'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-PermalinkJS
+HotlinkJS
 ===========
 
-Lightweight JavaScript library to get permalinks for files hosted on cloud storage services such as Google Drive or Dropbox.
+Lightweight JavaScript library to get hotlinks (direct links) for files hosted on cloud storage services such as Google Drive or Dropbox.
--- hotlink.js
+++ hotlink.js
@@ -1,19 +1,19 @@
-var drivePermalinkPattern = "https://drive.google.com/uc?export=view&id={fileId}";
+var driveHotlinkPattern = "https://drive.google.com/uc?export=view&id={fileId}";
 var driveSourcePattern = /^https:\/\/drive\.google\.com\/file\/d\/([0-9a-zA-Z_-]+)\/edit\?usp=sharing$/i;
 
-function generatePermalink(url) {
-    var permalink;
+function generateHotlink(url) {
+    var hotlink;
 
     // If URL matches Drive's sharing URL pattern
     if (driveSourcePattern.test(url)) {
         var fileId = RegExp.$1;
 
-        permalink = drivePermalinkPattern.replace("\{fileId\}", fileId);
+        hotlink = driveHotlinkPattern.replace("\{fileId\}", fileId);
     }
     else {
         // Unknown URL pattern
-        permalink = null;
+        hotlink = null;
     }
 
-    return permalink;
+    return hotlink;
 }
