98722aab605bf039fd31aa69b132d044f65af025
Mickael Allonneau <mickael.allonneau@gmail.com>
2014-04-24 17:31:14 +0000

    hotlink.js> added support for Dropbox share links

--- hotlink.js
+++ hotlink.js
@@ -1,5 +1,7 @@
-var driveHotlinkPattern = "https://drive.google.com/uc?export=view&id={fileId}";
+var driveHotlinkPattern = "https://drive.google.com/uc?export=view&id=<fileId>";
 var driveSourcePattern = /^https:\/\/drive\.google\.com\/file\/d\/([0-9a-zA-Z_-]+)\/edit\?usp=sharing$/i;
+var dropboxHotlinkPattern = "https://dl.dropbox.com/s/<fileId>/<fileName>";
+var dropboxSourcePattern = /^https:\/\/www\.dropbox\.com\/s\/([0-9a-z]+)\/(.*)+$/i;
 
 function generateHotlink(url) {
     var hotlink;
@@ -8,7 +10,15 @@ function generateHotlink(url) {
     if (driveSourcePattern.test(url)) {
         var fileId = RegExp.$1;
 
-        hotlink = driveHotlinkPattern.replace("\{fileId\}", fileId);
+        hotlink = driveHotlinkPattern.replace("<fileId>", fileId);
+    }
+    // If URL matches Dropbox's sharing URL pattern
+    else if (dropboxSourcePattern.test(url)) {
+        var fileId = RegExp.$1;
+        var fileName = RegExp.$2;
+
+        hotlink = dropboxHotlinkPattern.replace("<fileId>", fileId);
+        hotlink = hotlink.replace("<fileName>", fileName);
     }
     else {
         // Unknown URL pattern
@@ -17,3 +27,4 @@ function generateHotlink(url) {
 
     return hotlink;
 }
+
