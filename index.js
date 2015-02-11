388ec706d99fa41ca75e7e5eae544f56975ae35e
Mickael Allonneau <mickael.allonneau@gmail.com>
2015-02-11 22:27:03 +0100

    Rewrote 'index.js' using 'form-controller.js'

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
+function isValidURL(str) {
+  return driveSourcePattern.test(str)
+    || dropboxSourcePattern.test(str)
+  ;
+}
+
 jQuery(function($) {
+  var hotlinkUrl = $("#hotlinkUrl");
+  var message = $("#message");
+  var sourceUrl = $("#sourceUrl");
+
+  /* Init the 'Copy' button */
   $("#copyHotlink").clipboard({
     path: 'vendor/jquery.clipboard.swf',
     copy: function() {
-      return $("#hotlinkUrl").val();
+      return hotlinkUrl.val();
     }
   });
-  $("#sourceUrl").keyup(function() {
-    var url = $("#sourceUrl").val();
 
-    if (url == "") {
-      $("#message").hide("fast");
-      $("#sourceUrl").parent().removeClass("has-error has-success");
+  var controller = new FormController('.form');
 
-      return;
-    }
+  controller.addInputCallback(function(event) {
+    var url = event.input.value;
 
-    var hotlink = generateHotlink(url);
+    // Source URL matches rule(s) ?
+    if (controller.isValid()) {
+      var hotlink = generateHotlink(url);
 
-    if (!hotlink) {
-      $("#message").show("fast");
-      $("#sourceUrl").parent().addClass("has-error");
-      $("#sourceUrl").parent().removeClass("has-success");
+      // Show hotlink URL
+      hotlinkUrl.val(hotlink);
+      // Hide error message
+      message.hide("fast");
+      // Change outline color
+      sourceUrl.parent().addClass("has-success");
+      sourceUrl.parent().removeClass("has-error");
     }
     else {
-      $("#message").hide("fast");
-      $("#hotlinkUrl").val(hotlink);
-      $("#sourceUrl").parent().addClass("has-success");
-      $("#sourceUrl").parent().removeClass("has-error");
+      if (! url) {
+        // Hide error message
+        message.hide("fast");
+        // No outline color
+        sourceUrl.parent().removeClass("has-error has-success");
+      }
+      else {
+        hotlinkUrl.val('');
+        message.show("fast");
+        sourceUrl.parent().addClass("has-error");
+        sourceUrl.parent().removeClass("has-success");
+      }
     }
   });
+  controller.getInput = function(property) {
+    return document.querySelector('#sourceUrl');
+  };
+  controller.getProperty = function(input) {
+    return 'source';
+  };
+  controller.setModel({ source: null });
+  controller.setRules({ source: isValidURL });
+  controller.setUp();
 });
